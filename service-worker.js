dea535a878c0eb307059338d8630da7956f96657
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-03-11 16:13:38 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,12 @@
 //This will make sure that pages load with a 200 OK response when offline
+self.addEventListener('install', (event) => {
+  console.log('Inside the install handler:', event);
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Inside the activate handler:', event);
+});
+
+self.addEventListener(fetch, (event) => {
+  console.log('Inside the fetch handler:', event);
+});
